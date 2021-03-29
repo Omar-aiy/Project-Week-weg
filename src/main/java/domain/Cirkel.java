caa8ea235a60d386a58b6bea098abe14bb9588b8
Omar <omar.aiyyachi@gmail.com>
2021-03-29 10:39:21 +0200

    remove Test

--- src/main/java/domain/Cirkel.java
+++ src/main/java/domain/Cirkel.java
@@ -1,4 +1,43 @@
 package domain;
 
+import java.awt.*;
+import java.util.Objects;
+
 public class Cirkel {
+    private Point middelpunt;
+    private double straal;
+
+    public Cirkel(Point middelpunt, double straal) {
+        if (middelpunt == null) throw new IllegalArgumentException();
+        if (straal <= 0) throw new IllegalArgumentException();
+        if (straal > Math.min(middelpunt.getX(), middelpunt.getY())) throw new IllegalArgumentException();
+        this.middelpunt = middelpunt;
+        this.straal = straal;
+    }
+
+    public Point getMiddelpunt() {
+        return middelpunt;
+    }
+
+    public double getStraal() {
+        return straal;
+    }
+
+    public double getOppervlakte() {
+        return Math.PI * straal * straal;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cirkel cirkel = (Cirkel) o;
+        return Double.compare(cirkel.straal, straal) == 0 && Objects.equals(middelpunt, cirkel.middelpunt);
+    }
+
+    @Override
+    public String toString() {
+        String output = "Cirkel met middelpunt " + getMiddelpunt().toString() + " en straal " + getStraal();
+        return output;
+    }
 }
